e58786ff7f282b022146b701129744ee73d695d0
Sa-Yoor Headley <sai.h152015@gmail.com>
2022-01-13 17:26:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -44,9 +44,7 @@ form.addEventListener('submit', (event) =>{
         setSuccessFor(confirmPassword)
     }
 
-    showSuccessMessage(username)
-    //Stop from submitting
-    //event.preventDefault()
+    event.preventDefault()
 })
 
 //Set Error Class
@@ -68,9 +66,4 @@ function setSuccessFor(field){
 function isEmail(email){
     const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     return emailRegex.test(email)
-}
-
-//Show Success Message
-function showSuccessMessage(username){
-    
 }
